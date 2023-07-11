8f72113104100519e1ba2556eedf4cfee34986c1
Janani <gjanani91@gmail.com>
2023-07-11 17:14:23 +0530

    adding files

---
+++ myscript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Checked";
+    document.getElementById("demo").style.fontSize="40px";
+    document.getElementById("demo").style.color="blue";
+    document.getElementById("demo").style.fontFamily="Monospace";
+
+  }

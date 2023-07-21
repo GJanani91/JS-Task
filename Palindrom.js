8085a5e56e6f87d6d7f055cf174640b6a0e7b6f0
Janani <gjanani91@gmail.com>
2023-07-21 15:13:57 +0530

    js file

---
+++ Palindrom.js
@@ -0,0 +1,12 @@
+let str=('madam');
+checkPalindrome(str);
+function checkPalindrome(str) {
+    let x = str.split();
+    let y = x.reverse();
+    let z = y.join();
+   /* if(str===z)//(str===z)?console.log("true"):colsole.log("false");
+    console.log("true");
+    else
+    console.log("false");*/
+(str===z)?console.log("true"):colsole.log("false");
+  }

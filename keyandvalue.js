3c6b1d774187f226f98612d87a905b05912c25df
Janani <gjanani91@gmail.com>
2023-07-25 14:10:11 +0530

    value defined

--- keyandvalue.js
+++ keyandvalue.js
@@ -1,14 +1,18 @@
-const person = ({
+const person = {
     firstName: "John",
     lastName: "Doe",
     age: 50,
     MobileNo: 123456789, 
-  });
+  };
+  var keys="";
+  var values="";
   keyAndValues();
   function keyAndValues(){
-  const keys = Object.keys(person);
-  const values=Object.values(person);
+  keys = Object.keys(person);
+  values=Object.values(person);
+
   }
   console.log("The keys are: "+keys);
   console.log("The values are: "+values);
+
   

9ab50f17e8b86dd813068b96382e39524d4c0db4
Janani <gjanani91@gmail.com>
2023-07-26 10:47:47 +0530

    lonest Index

---
+++ longestIndex.js
@@ -0,0 +1,16 @@
+const x=["Hello","World","Hai"];
+let y=0;
+let z;
+
+function longestIndex(){
+for(let i=0;i<x.length;i++){
+if(x[i].length>y) //if(x[i].length>=0) output:World
+{
+     y =x[i].length;
+      z = x[i];
+
+}
+}
+}
+longestIndex();
+console.log(""+ z);

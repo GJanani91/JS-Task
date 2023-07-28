7a80a4b6772cbc341b9db5d868c81f5ab82f8002
Janani <gjanani91@gmail.com>
2023-07-28 14:22:09 +0530

    add delete function

--- arrayofstudents.js
+++ arrayofstudents.js
@@ -5,7 +5,7 @@ let student = [
     { id: 4,Name:"Ram",total:100,Gender:"Male"  }
     ];
     //Finding student name by id
-   findStudentByid();
+  findStudentByid();
     function findStudentByid(){
         for (i=0;i<student.length;i++){
 if (student[i].id >1) 
@@ -30,11 +30,20 @@ console.log("Added new student details to the Student details list: ")
 console.log(student);
 }
 //deleting object by giving id
-/*deleteStudentByid()
+deleteStudentByid()
 function deleteStudentByid(id){
-    delete student.id[0];
-    console.log(student);
-}*/
+    for(i=0;i<student.length;i++){
+        if(student[i].id===2){
+            let splice_value =student.splice(i,1);
+            console.log("the deleted student by id: ");
+            console.log(splice_value);
+            console.log("the remaining student detaisls are: ");
+            console.log(student);
+        }
+
+    
+    }
+}
 //to print the students details in a row
 function printStudentRow(details) {
     console.log("id: "+details.id, "Name: "+details.Name, "Total: "+details.total,"Gender: "+details.Gender);

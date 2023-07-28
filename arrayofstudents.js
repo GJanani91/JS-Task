ac54bf6567cd47d6aa5b5f343a7eade5fb50e4bc
Janani <gjanani91@gmail.com>
2023-07-28 12:29:51 +0530

    array and object

---
+++ arrayofstudents.js
@@ -0,0 +1,45 @@
+let student = [
+    { id: 1,Name:"Janani",total:100,Gender:"Female" },
+    { id: 2,Name:"Kumar", total:100,Gender:"Male" },
+    { id: 3,Name:"Priya",total:100,Gender:"Female"  },
+    { id: 4,Name:"Ram",total:100,Gender:"Male"  }
+    ];
+    //Finding student name by id
+   findStudentByid();
+    function findStudentByid(){
+        for (i=0;i<student.length;i++){
+if (student[i].id >1) 
+    console.log("The Name of the student found-  "+student[i].Name);
+}
+}
+//Finding students name by giving gender
+findStudentByGender();
+function findStudentByGender(){
+    for (i=0;i<student.length;i++){
+        if (student[i].Gender==="Male")
+console.log("The Gender- Male of the student found-  "+student[i].Name);
+    }
+}
+//adding object to the set of objects
+includeStudent();
+function includeStudent(){
+
+let add={id: 5,Name:"Santhosh",total:40,Gender:"Male"  }
+student.push(add);
+console.log("Added new student details to the Student details list: ")
+console.log(student);
+}
+//deleting object by giving id
+/*deleteStudentByid()
+function deleteStudentByid(id){
+    delete student.id[0];
+    console.log(student);
+}*/
+//to print the students details in a row
+function printStudentRow(details) {
+    console.log("id: "+details.id, "Name: "+details.Name, "Total: "+details.total,"Gender: "+details.Gender);
+  }
+  console.log("List of Students:");
+  student.forEach(printStudentRow);
+
+                                                 
---
+++ arrayoperations.js
@@ -0,0 +1,17 @@
+const numbers=[1,2,3,4,5,6,7,8,9];
+console.log("The given numbers: "+numbers);
+//Adding element at start of array
+numbers.unshift(0);
+console.log("Adding number at start of array: "+numbers );
+//Removing last elememt of array
+numbers.pop();
+console.log("Removing last element of array: "+numbers );
+//Adding element at second index position
+numbers[2] = 654;
+console.log("Adding element at second index position: "+numbers );
+//Removing first element
+numbers.shift();
+console.log("Removing first element in an array: "+numbers );
+//adding elememt at last position
+numbers.push(9);
+console.log("adding element at last position of array: "+numbers );
